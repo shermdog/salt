1c1b3c820b8de0b16e9c340fa03eda8ac93b2c74
Rick Sherman <rick@shermdog.com>
2015-07-28 16:42:02 -0500

    Initial work

--- salt/modules/junos.py
+++ salt/modules/junos.py
@@ -9,20 +9,26 @@ from __future__ import absolute_import
 
 # Import python libraries
 import logging
+import re
+import json
 
 # Juniper interface libraries
 # https://github.com/Juniper/py-junos-eznc
-
-
 try:
     # pylint: disable=W0611
     import jnpr.junos
     import jnpr.junos.utils
     import jnpr.junos.cfg
+    from jnpr.junos.utils.sw import SW
+    from jnpr.junos.exception import LockError, UnlockError, ConfigLoadError
+    from jnpr.junos.version import VERSION
     # pylint: enable=W0611
-    HAS_JUNOS = True
+    if not float(re.match('\d+.\d+', VERSION).group()) >= 1.2:
+        HAS_PYEZ = False
+    else:
+        HAS_PYEZ = True
 except ImportError:
-    HAS_JUNOS = False
+    HAS_PYEZ = False
 
 
 # Set up logging
@@ -40,7 +46,7 @@ def __virtual__():
     module to work.  We also need a proxymodule entry in __opts__
     in the opts dictionary
     '''
-    if HAS_JUNOS and 'proxy' in __opts__:
+    if HAS_PYEZ and 'proxymodule' in __opts__:
         return __virtualname__
     else:
         return False
@@ -55,26 +61,80 @@ def facts_refresh():
     return __opts__['proxymodule']['junos.conn']().refresh()
 
 
-def set_hostname(hostname=None, commit_change=True):
-
-    ret = dict()
+def lock():
     conn = __opts__['proxymodule']['junos.conn']()
-    if hostname is None:
+    ret = dict()
+
+    log.debug('taking lock')
+    try:
+        conn.cu.lock()
+        log.debug('locked config')
+        ret['out'] = True
+        ret['msg'] = 'locked config'
+    except LockError:
+        log.debug('could not lock config!')
+        ret['out'] = False
+        ret['msg'] = 'could not lock config!'
+
+    return ret
+
+
+def rpc(rpc_cmd, **kwargs):
+    conn = __opts__['proxymodule']['junos.conn']()
+    try:
+        return json.dumps(conn.rpc.__getattr__(rpc_cmd)(**kwargs))
+    except Exception as e:
+        ret = dict()
+        ret['msg'] = e
         ret['out'] = False
         return ret
 
-    # Added to recent versions of JunOs
-    # Use text format instead
-    set_string = 'set system host-name {0}'.format(hostname)
 
-    conn.cu.load(set_string, format='set')
-    if commit_change:
-        return commit()
-    else:
+def unlock():
+    conn = __opts__['proxymodule']['junos.conn']()
+    ret = dict()
+
+    log.debug('unlocking config')
+    try:
+        conn.cu.unlock()
+        log.debug('unlocked config')
         ret['out'] = True
-        ret['msg'] = 'set system host-name {0} is queued'.format(hostname)
+        ret['msg'] = 'unlocked config'
+    except UnlockError:
+        log.debug('could not unlock config!')
+        ret['out'] = False
+        ret['msg'] = 'could not unlock config!'
 
     return ret
+
+
+def load_config(*vargs, **kwargs):
+    conn = __opts__['proxymodule']['junos.conn']()
+    ret = dict()
+
+    lock()
+
+    try:
+        log.debug('Loading Configuration')
+        conn.cu.load(*vargs, **kwargs)
+        commit()
+        ret['out'] = True
+        ret['msg'] = 'Successfully loaded configuration'
+    except Exception as e:
+        ret['out'] = False
+        error_msg = 'Failed to load config: {0}'.format(e)
+        log.error(error_msg)
+        ret['msg'] = error_msg
+        unlock()
+
+    unlock()
+
+    return ret
+
+
+def commit_check():
+    conn = __opts__['proxymodule']['junos.conn']()
+    return conn.cu.commit_check()
 
 
 def commit():
@@ -82,10 +142,10 @@ def commit():
     conn = __opts__['proxymodule']['junos.conn']()
 
     ret = {}
-    commit_ok = conn.cu.commit_check()
+    commit_ok = commit_check()
     if commit_ok:
         try:
-            conn.cu.commit(confirm=True)
+            conn.cu.commit()
             ret['out'] = True
             ret['message'] = 'Commit Successful.'
         except Exception as e:
@@ -98,11 +158,26 @@ def commit():
     return ret
 
 
-def rollback():
+def shutdown(reboot=False, in_min=0, at=None):
+    conn = __opts__['proxymodule']['junos.conn']()
+    ret = {}
+
+    sw = SW(conn)
+    if reboot is True:
+        ret['message'] = 'Reboot in: {0}, at: {1}'.format(in_min, at)
+        sw.reboot(in_min, at)
+    else:
+        ret['message'] = 'Shutdown in: {0}'.format(in_min)
+        sw.poweroff(in_min)
+    ret['out'] = True
+    return ret
+
+
+def rollback(rb_id=0):
     conn = __opts__['proxymodule']['junos.conn']()
     ret = dict()
 
-    ret['out'] = conn.cu.rollback(0)
+    ret['out'] = conn.cu.rollback(rb_id=rb_id)
 
     if ret['out']:
         ret['message'] = 'Rollback successful'
@@ -123,8 +198,5 @@ def diff():
 
 
 def ping():
-
-    ret = dict()
     conn = __opts__['proxymodule']['junos.conn']()
-    ret['message'] = conn.cli('show system uptime')
-    ret['out'] = True
+    return conn.probe()
